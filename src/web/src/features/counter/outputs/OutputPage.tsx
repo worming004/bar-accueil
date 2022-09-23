fe72de33aaf0fdae47c09fe7b6c487dda0635327
Mathieu Scolas <worming@hotmail.be>
2022-09-23 14:05:57 +0200

    add euro symbol

--- src/web/src/features/counter/outputs/OutputPage.tsx
+++ src/web/src/features/counter/outputs/OutputPage.tsx
@@ -54,6 +54,6 @@ function TokenPresentation(props: TokenWithCount) {
 function AmountPresentation(props: { amount: number }) {
     return (<>
         <h1 className='text-4xl'>Montant:</h1>
-        <span className='text-4xl'>{props.amount.toFixed(2)}</span>
+        <span className='text-4xl'>{props.amount.toFixed(2)}€</span>
     </>)
 }
