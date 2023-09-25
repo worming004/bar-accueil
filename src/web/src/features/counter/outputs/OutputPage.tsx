9502fa210ccc3551161fa0391248aeecf4ea17ae
Mathieu Scolas <worming@hotmail.be>
2023-09-25 07:37:21 +0200

    put key in list

--- src/web/src/features/counter/outputs/OutputPage.tsx
+++ src/web/src/features/counter/outputs/OutputPage.tsx
@@ -1,65 +1,66 @@
-import {useAppSelector} from "../../../app/hooks";
-import {selectPresentationAmount, selectPresentationTokens, TokenWithCount} from "../counterSlice";
-import {GetTokenColor} from "../../tokens";
+import { useAppSelector } from "../../../app/hooks";
+import { selectPresentationAmount, selectPresentationTokens, TokenWithCount } from "../counterSlice";
+import { GetTokenColor } from "../../tokens";
 
 export function OutputPage() {
-    const tokens = useAppSelector(selectPresentationTokens);
-    const amount = useAppSelector(selectPresentationAmount);
-    const tokensHtml = tokens.map(TokenPresentation)
-    return (<>
-        <div className="flex flex-wrap">
-            {tokensHtml}
-        </div>
-        <AmountPresentation amount={amount}></AmountPresentation>
-    </>)
+  const tokens = useAppSelector(selectPresentationTokens);
+  const amount = useAppSelector(selectPresentationAmount);
+  const tokensHtml = tokens.map(TokenPresentation)
+  return (<>
+    <div className="flex flex-wrap">
+      {tokensHtml}
+    </div>
+    <AmountPresentation amount={amount}></AmountPresentation>
+  </>)
 }
 
 function TokenPresentation(props: TokenWithCount) {
-    const commonStyle = {
-        fontSize: '35px',
-        width: "80px",
-        height: "80px",
-        backgroundColor: GetTokenColor(props)
-    }
-    const roundStyle = {
-        ...commonStyle,
-        alignItems: 'flex-start',
-    }
-    const cardStyle = {
-        ...commonStyle
-    }
-    const commonClass = 'flex h-screen '
-    const roundClass = commonClass + 'rounded-full'
-    const cardClass = commonClass + ''
-    const elementClassName = 'm-auto'
-    const boxStyle = {
-        margin: "25px",
-    }
+  const commonStyle = {
+    fontSize: '35px',
+    width: "80px",
+    height: "80px",
+    backgroundColor: GetTokenColor(props)
+  }
+  const roundStyle = {
+    ...commonStyle,
+    alignItems: 'flex-start',
+  }
+  const cardStyle = {
+    ...commonStyle
+  }
+  const commonClass = 'flex h-screen '
+  const roundClass = commonClass + 'rounded-full'
+  const cardClass = commonClass + ''
+  const elementClassName = 'm-auto'
+  const boxStyle = {
+    margin: "25px",
+  }
 
-    const selectedStyle = props.shape === 'round' ? {
-        style: roundStyle,
-        class: roundClass
-    } : props.shape === 'card' ? {
-        style: cardStyle,
-        class: cardClass
-    } : undefined
-    return (<div style={boxStyle}>
-        <div
-            key={props.name}
-            style={selectedStyle?.style}
-            className={selectedStyle?.class}
-        >
-            <span className={elementClassName}>{props.count}</span>
-        </div>
-        <div className="m-auto text-lg">
-        {props.value}€
-        </div>
-    </div>)
+  const selectedStyle = props.shape === 'round' ? {
+    style: roundStyle,
+    class: roundClass
+  } : props.shape === 'card' ? {
+    style: cardStyle,
+    class: cardClass
+  } : undefined
+  return (<div
+    key={props.name}
+    style={boxStyle}>
+    <div
+      style={selectedStyle?.style}
+      className={selectedStyle?.class}
+    >
+      <span className={elementClassName}>{props.count}</span>
+    </div>
+    <div className="m-auto text-lg">
+      {props.value}€
+    </div>
+  </div>)
 }
 
 function AmountPresentation(props: { amount: number }) {
-    return (<>
-        <h1 className='text-4xl'>Montant:</h1>
-        <span className='text-4xl'>{props.amount.toFixed(2)}€</span>
-    </>)
+  return (<>
+    <h1 className='text-4xl'>Montant:</h1>
+    <span className='text-4xl'>{props.amount.toFixed(2)}€</span>
+  </>)
 }
