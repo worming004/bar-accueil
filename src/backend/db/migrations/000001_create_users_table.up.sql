8e8a0f51a66b746be7d17e952ed3af5e0c4b4766
Mathieu Scolas <worming@hotmail.be>
2024-09-25 21:07:57 +0200

    rm backend things

--- src/backend/db/migrations/000001_create_users_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS users(
-   user_id serial PRIMARY KEY,
-   username VARCHAR (100) UNIQUE NOT NULL,
-  -- TODO Salt
-   password VARCHAR (100) NOT NULL,
-   email VARCHAR (300) UNIQUE NOT NULL
-);
